5c6cb51c1ab11c0b4937d986b094f273f17d8f53
Travis Newton <tnewton1@gmail.com>
2018-09-01 16:45:16 -0400

    checkpoint

--- pets.py
+++ pets.py
@@ -3,5 +3,5 @@ def describe_pet(animal_type, pet_name):
 	print("\nI have a " + animal_type + ".")
 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet('hamster', 'harry')
+describe_pet(animal_type='hamster', pet_name='harry')
 describe_pet('dog', 'willie')
